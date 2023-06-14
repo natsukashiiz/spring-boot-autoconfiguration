669215519e1951eb3cfbff67187753b6692ffb47
vv2dev <114040584+vv2dev@users.noreply.github.com>
2023-06-14 15:54:40 +0700

    Initial commit

---
+++ src/main/java/com/natsukashiiz/autoconfiguration/SpringBootAutoconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.natsukashiiz.autoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoconfigurationApplication.class, args);
+    }
+
+}
